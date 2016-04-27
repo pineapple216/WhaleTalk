12667e20fc318242a6e6fbaba6b2891ac4358552
Koen Hendriks <koenhendriks89@gmail.com>
2016-04-27 12:36:49 +0200

    Implemented a ChatCreationDelegate to manage multiple NSManagedObjectContext objects.

---
+++ WhaleTalk/ChatCreationDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  ChatCreationDelegate.swift
+//  WhaleTalk
+//
+//  Created by Koen Hendriks on 27/04/16.
+//  Copyright © 2016 Koen Hendriks. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+protocol ChatCreationDelegate {
+    
+    func created(chat: Chat, inContext context: NSManagedObjectContext)
+}
