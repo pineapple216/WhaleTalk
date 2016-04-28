58a9d022736768ab0e4d9fb9ec1f2d83ba10ba90
Koen Hendriks <koenhendriks89@gmail.com>
2016-04-28 20:20:16 +0200

    Added a VC to add participants to a chat group.

---
+++ WhaleTalk/NewGroupParticipantsViewController.swift
@@ -0,0 +1,90 @@
+//
+//  NewGroupParticipantsViewController.swift
+//  WhaleTalk
+//
+//  Created by Koen Hendriks on 28/04/16.
+//  Copyright © 2016 Koen Hendriks. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class NewGroupParticipantsViewController: UIViewController {
+
+    var context:NSManagedObjectContext?
+    var chat: Chat?
+    var chatCreationDelegate: ChatCreationDelegate?
+    
+    private var searchField: UITextField!
+    
+    private let tableView = UITableView(frame: CGRectZero, style: .Plain)
+    private let cellIdentifier = "ContactCell"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Add Participants"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Create", style: .Plain, target: self, action: "createChat")
+        showCreateButton(false)
+        
+        automaticallyAdjustsScrollViewInsets = false
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.tableFooterView = UIView(frame: CGRectZero)
+        
+        searchField = createSearchField()
+        
+        tableView.tableHeaderView = searchField
+        
+        fillViewWith(tableView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func createSearchField() -> UITextField {
+        let searchField = UITextField(frame: CGRect(x: 0, y: 0, width: 0, height: 50))
+        searchField.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
+        searchField.placeholder = "Type contact name"
+        
+        let holderView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        searchField.leftView = holderView
+        searchField.leftViewMode = .Always
+        
+        let image = UIImage(named: "contact_icon")?.imageWithRenderingMode(.AlwaysTemplate)
+        
+        let contactimage = UIImageView(image: image)
+        contactimage.tintColor = UIColor.darkGrayColor()
+        
+        holderView.addSubview(contactimage)
+        contactimage.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraints:[NSLayoutConstraint] = [
+            contactimage.widthAnchor.constraintEqualToAnchor(holderView.widthAnchor, constant: -20),
+            contactimage.heightAnchor.constraintEqualToAnchor(holderView.heightAnchor, constant: -20),
+            contactimage.centerXAnchor.constraintEqualToAnchor(holderView.centerXAnchor),
+            contactimage.centerYAnchor.constraintEqualToAnchor(holderView.centerYAnchor)
+        ]
+        NSLayoutConstraint.activateConstraints(constraints)
+        
+        return searchField
+    }
+
+    private func showCreateButton(show: Bool){
+        if show {
+            navigationItem.rightBarButtonItem?.tintColor = view.tintColor
+            navigationItem.rightBarButtonItem?.enabled = true
+        }
+        else{
+            navigationItem.rightBarButtonItem?.tintColor = UIColor.lightGrayColor()
+            navigationItem.rightBarButtonItem?.enabled = false
+        }
+    }
+    
+    
+    
+}
--- WhaleTalk/NewGroupViewController.swift
+++ WhaleTalk/NewGroupViewController.swift
@@ -70,6 +70,13 @@ class NewGroupViewController: UIViewController {
     func next() {
         guard let context = context, chat = NSEntityDescription.insertNewObjectForEntityForName("Chat", inManagedObjectContext: context) as? Chat else {return}
         chat.name = subjectField.text
+        
+        let vc = NewGroupParticipantsViewController()
+        vc.context = context
+        vc.chat = chat
+        vc.chatCreationDelegate = chatCreationDelegate
+        
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     func updateCharacterLabel(forCharCount length: Int){
