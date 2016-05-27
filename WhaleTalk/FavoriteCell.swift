c405e4ce67ff624b47f9738ecfcafb814fbb28b0
Koen Hendriks <koenhendriks89@gmail.com>
2016-05-27 18:29:42 +0200

    Implemented a FavoritesViewController to view favourite contacts.

---
+++ WhaleTalk/FavoriteCell.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteCell.swift
+//  WhaleTalk
+//
+//  Created by Koen Hendriks on 25/05/16.
+//  Copyright © 2016 Koen Hendriks. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    let phoneTypeLabel = UILabel()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .Subtitle, reuseIdentifier: reuseIdentifier)
+        
+        detailTextLabel?.textColor = UIColor.lightGrayColor()
+        phoneTypeLabel.textColor = UIColor.lightGrayColor()
+        phoneTypeLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(phoneTypeLabel)
+        
+        phoneTypeLabel.centerYAnchor.constraintEqualToAnchor(contentView.centerYAnchor).active = true
+        phoneTypeLabel.trailingAnchor.constraintEqualToAnchor(contentView.layoutMarginsGuide.trailingAnchor).active = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
