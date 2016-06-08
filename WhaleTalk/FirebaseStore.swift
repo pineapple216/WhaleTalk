1206a95be94be8eae17644c76705d427f90f3e5d
Koen Hendriks <koenhendriks89@gmail.com>
2016-06-08 17:56:21 +0200

    Refactoring and implemented firebase signup.

--- WhaleTalk/FirebaseStore.swift
+++ WhaleTalk/FirebaseStore.swift
@@ -12,7 +12,16 @@ import CoreData
 
 class FirebaseStore {
     private let context: NSManagedObjectContext
-    private let rootRef = Firebase(url: "https://koen-whaletalk.firebaseapp.com")
+    private let rootRef = Firebase(url: "https://koen-whaletalk.firebaseio.com")
+    
+    private var currentPhoneNumber: String? {
+        set(phoneNumber){
+            NSUserDefaults.standardUserDefaults().setObject(phoneNumber, forKey: "phoneNumber")
+        }
+        get{
+            return NSUserDefaults.standardUserDefaults().objectForKey("phoneNumber") as? String
+        }
+    }
     
     init(context: NSManagedObjectContext){
         self.context = context
@@ -22,3 +31,43 @@ class FirebaseStore {
         return rootRef.authData != nil
     }
 }
+
+extension FirebaseStore: RemoteStore{
+    func startSyncing() {
+        
+    }
+    
+    func store(inserted inserted: [NSManagedObject], updated: [NSManagedObject], deleted: [NSManagedObject]) {
+        
+    }
+    
+    func signUp(phonenumber phoneNumber: String, email: String, password: String, succes: () -> (), error errorCallback: (errorMessage: String) -> ()) {
+        
+        rootRef.createUser(email, password: password, withValueCompletionBlock: {
+            error, result in
+            if error != nil{
+                errorCallback(errorMessage: error.description)
+            }
+            else{
+                let newUser = [
+                    "phoneNumber" : phoneNumber
+                ]
+                self.currentPhoneNumber = phoneNumber
+                let uid = result["uid"] as! String
+                self.rootRef.childByAppendingPath("users").childByAppendingPath("uid").setValue(newUser)
+                self.rootRef.authUser(email, password: password, withCompletionBlock: {
+                    error, authData in
+                    
+                    if error != nil{
+                        errorCallback(errorMessage: error.description)
+                    }
+                    else{
+                        succes()
+                    }
+                })
+            }
+        })
+    }
+}
+
+
