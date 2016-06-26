664c759af604724952489ad0ef51a31ac2d01709
Koen Hendriks <koenhendriks89@gmail.com>
2016-06-26 15:41:46 +0200

    Added a new upload method to upload messages.

--- WhaleTalk/FirebaseModels.swift
+++ WhaleTalk/FirebaseModels.swift
@@ -50,6 +50,29 @@ extension Chat: FirebaseModel{
     }
 }
 
+extension Message: FirebaseModel{
+    func upload(rootRef: Firebase, context: NSManagedObjectContext) {
+        if chat?.storageId == nil{
+            chat?.upload(rootRef, context: context)
+        }
+        let data = [
+            "message"   : text!,
+            "sender"    : FirebaseStore.currentPhoneNumber!
+        ]
+        guard let chat = chat, timestamp = timestamp, storageId = chat.storageId else {return}
+        let timeInterval = String(Int(timestamp.timeIntervalSince1970 * 100000))
+        rootRef.childByAppendingPath("chats/"+storageId+"/messages/"+timeInterval).setValue(data)
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
 
